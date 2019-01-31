b74f5de28c703c531e18c25c836d73bac7e50417
Sampo Paukkonen <sampo.paukkonen@aalto.fi>
2019-01-31 09:10:02 +0200

    More progress in the Aparments algorithm

--- Apartments/Apartments.py
+++ Apartments/Apartments.py
@@ -2,24 +2,23 @@ from collections import deque
 from sys import stdin, stdout
 
 def main():
-    r = [int(x) for x in stdin.readline().split()] #The amount of applicants, the amount of apartments and delta
-    n = [int(x) for x in stdin.readline().split()].sort() #Size wishes of the applicants
-    m = [int(x) for x in stdin.readline().split()].sort() #Sizes of the apartments
-    if (n == m):
-        print(r[0])
-    else:
-        n = deque(n)
-        d = r[-1] #delta is the last element in r
-        counter = 0
-        while(m and n):
-            i = next((i for i,a in enumerate(m) if abs(n[0] - a) <= d), False) #Index of the apartment for the smallest size wish
-            if (i):
-                m.pop(i)
-                n.popleft()
-                counter += 1
-            else:
-                n.popleft()
-        print(counter)
+    r = [int(x) for x in stdin.readline().split()]        #The amount of applicants, the amount of apartments and delta
+    n = [int(x) for x in stdin.readline().split()]        #Size wishes of the applicants
+    m = [int(x) for x in stdin.readline().split()]        #Sizes of the apartments
+    n = deque(sorted(n))
+    m.sort()
+    d = r[-1] #delta is the last element in r
+    counter = 0
+    while(m and n):
+        i = next((i for i,a in enumerate(m) if abs(n[0] - a) <= d), "NotFound") #Index of the apartment for the smallest size wish
+        if (i != "NotFound"):
+            #print("{}".format(i))
+            m.pop(i)
+            n.popleft()
+            counter += 1
+        else:
+            n.popleft()
+    print(counter)
 main()
 
 """TODO: Currently seems to bug with the aparment selection. If delta is zero doens't act accordingly.
