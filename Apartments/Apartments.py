893eade78efe03172d6197cb5aa7198822d24b12
Sampo Paukkonen <sampo.paukkonen@aalto.fi>
2019-02-02 09:31:15 +0200

    More optimization for the Apartments

--- Apartments/Apartments.py
+++ Apartments/Apartments.py
@@ -1,18 +1,24 @@
-from collections import deque
+from collections import deque, OrderedDict
 from sys import stdin, stdout
 
 def main():
     r = [int(x) for x in stdin.readline().split()]        #The amount of applicants, the amount of apartments and delta
     n = [int(x) for x in stdin.readline().split()]        #Size wishes of the applicants
-    m = [int(x) for x in stdin.readline().split()]        #Sizes of the apartments
+    p = [int(x) for x in stdin.readline().split()]        #Sizes of the apartments
     n = deque(sorted(n))
-    m.sort()
+    p.sort()
+    m = OrderedDict()                                     #OrderedDict for the apartments with string indicies as keys
+    #m = OrderedDict(enumerate(sorted(m)))
+    if (p):                                               #To check if p has anything inside
+        for i in range(len(p) - 1):
+            m["{}".format(i)] = p[i]
     d = r[-1]                                             #delta is the last element in r
     counter = 0
     while(m and n):
-        i = next((i for i,a in enumerate(m) if abs(n[0] - a) <= d), "NotFound") #Index of the apartment for the smallest size wish
+        i = next((i for i,a in m.items() if abs(n[0] - a) <= d), "NotFound") #Index of the apartment for the smallest size wish
         if (i != "NotFound"):
-            m.pop(i)
+            del m[i]
+            #m.pop(i)
             n.popleft()
             counter += 1
         else:
@@ -21,4 +27,5 @@ def main():
 main()
 
 """TODO: Change the datas structure for example to dict of any that allows removing elements in O(1) time
-, or something similar."""
+, or something similar.
+Try using only regular dict since we will be adding the stuff we need in the order we want."""
